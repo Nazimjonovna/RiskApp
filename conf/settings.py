07a51e0ae8fd16b8076b522d78bb0ce91c0d35bd
Shahlo Turayeva <s.turayeva@eopc.uzcard.uz>
2026-03-25 15:34:04 +0500

    fghjk

--- conf/settings.py
+++ conf/settings.py
@@ -60,9 +60,9 @@ SWAGGER_SETTINGS = {
 }
 
 MIDDLEWARE = [
-    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
